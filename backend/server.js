46cd8cf931f3bfb8b2fa0e26d76cc954b38c572d
Vipul Chetan <vipulchetan.m24@iiits.in>
2026-03-23 11:43:31 +0530

    Fixed body parsing issue

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/codeSearchDB")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
@@ -39,4 +40,3 @@ app.get("/search", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-

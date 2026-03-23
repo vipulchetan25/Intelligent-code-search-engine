e91ddc562169964c84f381967d1b2de44f18444a
Vipul Chetan <vipulchetan.m24@iiits.in>
2026-03-23 10:09:04 +0530

    Added dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,6 @@ app.get("/search", async (req, res) => {
   res.json(results);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
